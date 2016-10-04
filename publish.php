6afce7f6e603d50c345091a50ef38d3f24a2c836
zhql0907 <zhql0907@outlook.com>
2016-10-04 10:42:00 +0800

    发布活动

--- publish.php
+++ publish.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello World";
-?>
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
