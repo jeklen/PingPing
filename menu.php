17c358ffa1fb148244f5dcbaf20094f9ad892868
zhql0907 <zhql0907@outlook.com>
2016-10-06 06:46:47 +0800

    测试不同的url

--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@ $menuPostData='{
 					 {	
 						  "type":"view",
 						  "name":"发布活动",
-                          "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx42dbf69f8939e9a8&redict_uri=http://lovepingping.applinzi.com/publish.php&response_type=code&scope=$snsapi_base&state=1"
+                          "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx42dbf69f8939e9a8&redict_uri=www.lovepingping.applinzi.com/publish.php&response_type=code&scope=$snsapi_base&state=1"
 					  },
 					  {
 						   "type":"click",
