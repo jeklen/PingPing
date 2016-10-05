6bc741b05a90f23b08a00770d5b8244c84a92949
zhql0907 <zhql0907@outlook.com>
2016-10-06 06:24:18 +0800

    增加 缺少的函数curl_get

--- weixin.class.php
+++ weixin.class.php
@@ -276,6 +276,25 @@ function curl_post($url,$data){
     return $tmpInfo; 
 }
 
+/**
+ * GET data
+ */
+function curl_get($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    if(!curl_exec($ch)){
+        error_log(curl_error($ch));
+        $data='';
+    }else{
+        $data = curl_multi_getcontent($ch);
+    }
+    curl_close($ch);
+    return $data;
+}
+
 
 
 /**
