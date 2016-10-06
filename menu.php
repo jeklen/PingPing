f7b6747b86c978970a1b11379392474afdaccc2d
zhql0907 <zhql0907@outlook.com>
2016-10-06 10:27:17 +0800

    fix error on redirect_url

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 require 'weixin.class.php';
 
 $open_url=urlencode('http://lovepingping.applinzi.com/publish.php');
-$redirect_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx42dbf69f8939e9a8&redirect_uri='.$redirect_url.'&response_type=code&scope=snsapi_base&state=1';
+$redirect_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx42dbf69f8939e9a8&redirect_uri='.$open_url.'&response_type=code&scope=snsapi_base&state=1';
 $ret=wxcommon::getToken();
 $ACCESS_TOKEN=$ret['access_token'];
 $menuPostData='{
