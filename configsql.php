eb4f9231449a49ad066809f864a3fb03e09321f3
zhql0907@outlook.com <zhql0907@outlook.com>
2016-10-26 20:13:01 +0800

    尝试输出数据库内容

--- configsql.php
+++ configsql.php
@@ -1,14 +1,4 @@
 <?php
-header("Content-Type:text/html;charset=utf-8");
-echo "用户名:".SAE_MYSQL_USER."<br>";
-echo "密码:". SAE_MYSQL_PASS.'<br>';
-echo "主库域名:".SAE_MYSQL_HOST_M."<br>";
-echo "从库域名:".SAE_MYSQL_HOST_S."<br>";
-echo "端口".SAE_MYSQL_PORT."<br>";
-echo "数据库名:".SAE_MYSQL_DB."<br>";
-?>
-
-<?php
 $hostname = SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT;
 $dbuser = SAE_MYSQL_USER;
 $dbpass = SAE_MYSQL_PASS;
--- join.php
+++ join.php
@@ -8,7 +8,22 @@ $openid = $token['openid'];
 echo $openid;
 ?>
 
-<?php 
-header("Content-Type:text/html;charset=utf-8");
-require("configsql.php");
- ?>
+<?php
+$mysql = new SaeMysql();
+//查询单条数据
+$sql = "select * from activity limit 1";
+$result = $mysql->getLine($sql);
+var_dump($result);
+//发现这个已经是按数组的方式返回的
+echo "<hr>";
+//查询多条数据
+$sql = "select * from activity";
+$mut_data = $mysql->getData($sql);
+var_dump($mut_data);
+//发现这个就是按二维数组输出的了，下面一个foreach输出
+echo "<hr>";
+foreach ($mut_data as $small) {
+	echo "活动".$small["activity_name"].'<br>';
+	echo "描述".$small["activity_describe"].'<br>';
+}
+?>
