5df47243a6ee2bc6b8e4e1daaf7ffe3fcc27db18
zhql0907@outlook.com <zhql0907@outlook.com>
2016-10-29 09:49:15 +0800

    try picture again

--- join.php
+++ join.php
@@ -31,8 +31,9 @@ echo $result['activity_describe'].'<br>';
 echo $result['user_name'].'<br>';
 }
 ?>
-    <img src="getImage.php?id=<?php echo $result['id']?>" alt="">
 
+<img src="getImage.php?id=1" alt=""/>
+//<img src="getImage.php?id=<?php echo $result['id']?>" alt="">
 <?php
 //查询单条数据
 //$sql = "select * from activity limit 1";
