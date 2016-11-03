9142957f266374de38e386f769631638b1a02d69
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-03 10:48:45 +0800

    改变编码

--- publish.php
+++ publish.php
@@ -14,6 +14,7 @@ echo $openid;
 <head>
     <title>发布活动</title>
     <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <meta charset="utf-8">
     <!--Bootstrap-->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
