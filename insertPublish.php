e98e4817e8086df536c26693d73f599092432921
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-07 21:54:04 +0800

    修改图片

--- insertPublish.php
+++ insertPublish.php
@@ -1,10 +1,8 @@
 <?php
 $mysql = new SaeMysql();
 
-if (!$mysql->setCharset("UTF8")) {
-    echo "can't set charset to utf8" . "<br>";
-}
-//$mysql->setCharset("UTF8");
+$mysql->setCharset("UTF8");
+
 var_dump($_POST);
 
 ?>
