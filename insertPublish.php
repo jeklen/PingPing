8c09bbaa47b39936ecbbd72e28ed95e53a338f94
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-15 09:14:28 +0800

    charset

--- insertPublish.php
+++ insertPublish.php
@@ -1,13 +1,10 @@
 <?php
+header("content-type:text/html; charset=utf8");
 session_start();
 echo session_id() . "<br>";
 $mysql = new SaeMysql();
 
 $mysql->setCharset("utf8");
-header("content-type:text/html; charset=utf8");
-
-
-print_r($_SERVER);
 
 var_dump($_POST);
 
