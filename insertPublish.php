c22cd5923de0afbfc393404bda8f5291f45b38fe
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-22 13:09:56 +0800

    change picutre size

--- insertPublish.php
+++ insertPublish.php
@@ -51,7 +51,7 @@ if (isset($_FILES['image']['tmp_name'])) {
     $img = new SaeImage();
     $img_data = file_get_contents($form_data);//获取本地上传的图片数据
     $img->setData($img_data);
-    $img->resize(180,180); //图片缩放为180*180
+    $img->resize(360,360); //图片缩放为180*180
     $img->improve();//提高图片质量的函数
     $new_data = $img->exec(); // 执行处理并返回处理后的二进制数据
     $s2->write('images',$name,$new_data);//将public修改为自己的storage 名称
