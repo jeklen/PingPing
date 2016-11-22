2686d1ed04363de7e73947c87bbfc5a739a61c2c
liu445126256 <445126256@qq.com>
2016-11-22 12:25:52 +0800

    修改join.php

--- join.php
+++ join.php
@@ -1,58 +1,251 @@
+<?php 
+  session_start();
+  require 'weixin.class.php'; 
+  $openid = 0;   
+  $token = wxmessage::getAuthToken($_GET['code']); 
+  $openid = $token['openid']; 
+  $_SESSION['user_id']=md5($openid);  
+ ?> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>大学生拼拼</title>
+<link rel="stylesheet" type="text/css" href="./group/1/multi/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./group/1/multi/css/bootstrap-responsive.css">
+<style>
+   div.span5.outlined{
+	   border:1px dotted black;
+   }
+   .navbar,.navbar-inner{
+	   height:10px;
+   }
+   
+</style>
+<script>
+ $(function () { 
+	$("#box").carousel();
+});
+</script>
+</head>
+<body>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="./group/1/multi/bootstrap.js"></script>
+
+
+
+
+<!--
+     //获取活动数目
+	$link=mysql_connect("localhost:3306","root");
+	mysql_select_db("activity") or die(mysql_error());
+	$sql1="select count(*) from table1";
+	$result1=mysql_query($sql1) or die(mysql_error());
+	$row1=mysql_fetch_array($result1);
+	$count=$row1[0];
+	//计算留言页数
+	$pagesize=5;
+	$totalpage=ceil($count / $pagesize);
+	if(isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= $totalpage){
+		$page=$_GET['page'];
+	}
+	else $page=1;
+	$start=($page-1)*$pagesize;
+	//获取活动内容
+	$sql2="select * from table1 order by activity_id desc limit $start,$pagesize";
+	$result2=mysql_query($sql2) or die(mysql_error());
+	
+-->
 <?php
-header("Content-Type: text/html;charset=utf-8"); 
-require 'weixin.class.php';
-$openid = 0;
-
-echo $_GET['code']."<br>";
-$token = wxmessage::getAuthToken($_GET['code']);
-$openid = $token['openid'];
-echo $openid;
-?>
-
-<?php
- header("Content-Type:text/html;charset=utf-8"); 
-$mysql = new SaeMysql();
-//mysql_query("set character set 'utf8'");//读库
-//mysql_query("set names 'utf8'");//写库
-//使用sae的库函数设置编码
-$mysql->setCharset("UTF8");
-
-$num_rec_per_page = 1;  //每页显示数量
-if (isset($_GET["page"])) {$page = $_GET["page"];} else {$page=1;}
-$start_from = ($page - 1) * $num_rec_per_page;
-
-$sqlOne = "SELECT activity.*, user.user_name
-	FROM activity, user
-	WHERE activity.id = activity_id_initiate
-	ORDER BY activity_time DESC 
-	LIMIT 1;";
-$result = $mysql->getLine($sqlOne);
-echo $result['activity_describe'].'<br>';
-echo $result['user_name'].'<br>';
-echo $result['id'].'<br>';
+    if(isset($openid)){
+    echo "<script>alert('欢迎'. $openid);</script>";
+    }
+    else echo	"<script>alert('user_id还是不行');</script>";
+    $link=new SaeMysql();
+    $sql1="select * from activity";
+	// $result1=$link->getLine($sql1);
+	//$count=$result1[0];
+    //$result = $runSql($sql1);
+    $result = $link->getData($sql1);
+    $count = 0;
+	foreach($result as $data ){
+		$count++;
+	}
+	//计算活动页数
+	$pagesize=5;
+	$totalpage=ceil($count / $pagesize);
+	if(isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= $totalpage){
+		$page=$_GET['page'];
+	}
+	else $page=1;
+	$start=($page-1)*$pagesize;
+	//获取活动内容
+	$sql2="select * from activity order by id desc limit $start,$pagesize";
+	$result2=$link->getData($sql2);
+	
 
 ?>
-
-
- <img src="getImage.php?id=1" alt="">
- <img src="getImage.php?id=".<?php echo $result['id']?> alt="no picture">
-//<img src="getImage.php?id=<?php echo $result['id']?>" alt="">
-<?php
-//查询单条数据
-//$sql = "select * from activity limit 1";
-//$result = $mysql->getLine($sql);
-//var_dump($result);
-//发现这个已经是按数组的方式返回的
-//echo "<hr>";
-//查询多条数据
-$sql = "select * from activity";
-$mut_data = $mysql->getData($sql);
-//var_dump($mut_data);
-//发现这个就是按二维数组输出的了，下面一个foreach输出
-echo "<hr>";
-foreach ($mut_data as $small) {
-	echo "活动".$small["activity_name"].'<br>';
-	echo "描述".$small["activity_place"].'<br>';
-    echo "<img src='getImage.php?id=". $small['id']."'". "alt='no picture'>";
-}
-?>
+    	
+   <div class="container-fluid" style="background-image:url(./group/1/multi/bgi.jpg);">
+      <ul class="nav nav-tabs">
+	     <!--判断哪个标签页显示-->
+	     <?php if(!isset($_GET['location']) || $_GET['location']!=2){ ?>
+	     <li class="active">
+		 <?php } else{?>
+		 <li>
+		 <?php }?>
+		
+		 <a href="#tab1" data-toggle="tab"><i class="icon-home"></i>首页</a></li>
+		 <!--判断哪个标签页显示-->
+		 <?php if(isset($_GET['location']) && $_GET['location']==2){ ?>
+	     <li class="active">
+		 <?php } else{?>
+		 <li>
+		 <?php }?>
+		 
+		 <a href="#tab2" data-toggle="tab"><i class="icon-heart"></i>活动</a></li>
+		 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i>我的<span class="caret"></span></a>
+		       <ul class="dropdown-menu">
+			      <li><a href="#">我发布的</a></li>
+				  <li><a href="./group/1/multi/my_activity.php">我参与的</a></li>
+			   </ul>
+		 </li>
+      </ul>
+	<div class="tab-content">
+	     <!--判断哪个标签页显示-->
+	     <?php if(!isset($_GET['location']) || $_GET['location']!=2){ ?>
+	    <div class="tab-pane active" id="tab1">
+		 <?php } else{?>
+		<div class="tab-pane" id="tab1">
+		 <?php }?>
+		    <div class="container">
+			    <div class="row">
+				    <div class="span12">
+					     <h1>大学生拼拼平台</h1>
+						 <div id="box" class="carousel slide" data-ride="carousel" data-interval="3000">
+						   <ol class="carousel-indicators">
+						      <li class="active" data-target="#box" data-slide-to="0"></li>
+							  <li data-target="#box" data-slide-to="1"></li>
+							  <li data-target="#box" data-slide-to="2"></li>
+						   </ol>
+						   <div class="carousel-inner">
+						      <div class="item">
+							     <img src="./group/1/multi/sample1.jpg" alt="图片无法显示">
+								 <div class="carousel-caption">
+								      <h4>你好</h4>
+									  <p>&nbsp;</p>
+									  <p>这里是大学生拼拼平台</p>
+									  <p>&nbsp;</p>
+									  <p>&nbsp;</p>
+								 </div>
+							  </div>
+							  <div class="item active">
+							     <img src="./group/1/multi/sample2.jpg" alt="图片无法显示">
+								 <div class="carousel-caption">
+								      <h4>你好</h4>
+									  <p>&nbsp;</p>
+									  <p>这里是大学生拼拼平台</p>
+									  <p>&nbsp;</p>
+									  <p>&nbsp;</p>
+								 </div>
+							  </div>
+							  <div class="item">
+							     <img src="./group/1/multi/sample3.jpg" alt="图片无法显示">
+								 <div class="carousel-caption">
+								      <h4>你好</h4>
+									  <p>&nbsp;</p>
+									  <p>这里是大学生拼拼平台</p>
+									  <p>&nbsp;</p>
+									  <p>&nbsp;</p>
+								 </div>
+							  </div>
+							</div>
+							<a class="left carousel-control" href="#box" data-slide="prev">&lsaquo;</a>
+							<a class="right carousel-control" href="#box" data-slide="next">&rsaquo;</a>
+						</div>
+						
+						 <a href="#" class="btn btn-info btn-large"><i class="icon-th-large icon-white"></i>了解更多</a>
+					</div>
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+					<div class="span12"><h2>热门活动</h2>
+					     <div class="row">
+						    <div class="span5 outlined"><p>热门活动1</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p></div>
+							<div class="span5 outlined"><p>热门活动2</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p></div>
+							<div class="span5 outlined"><p>热门活动3</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p></div>
+							<div class="span5 outlined"><p>热门活动4</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p></div>
+						 </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		 <!--判断哪个标签页显示-->
+	     <?php if(isset($_GET['location']) && $_GET['location']==2){ ?>
+	    <div class="tab-pane active" id="tab2">
+		 <?php } else{?>
+		  <div class="tab-pane" id="tab2">
+		 <?php }?>
+		    <div class="container">
+			    <div class="row">
+				    <div class="span12">
+					<div class="dropdown" style="text-align:right">
+					<a href="#" class="btn btn-large btn-info" data-toggle="dropdown">所有活动<span class="caret"></span></a>
+					<ul class="dropdown-menu pull-right">
+					    <li><a href="#">类型一</a></li>
+						<li><a href="#">类型二</a></li>
+					</ul>
+					</li>
+					</div>
+					</div>
+				</div>
+				<div>&nbsp;</div>
+				<?php foreach($result2 as $obj){ ?>
+				<div class="row">
+				    <div class="span2">
+					<?php  $id= $obj['id'];
+					       echo "<IMG SRC='./group/1/multi/getImage.php?id=$id' width=100% height=110%>"; ?>
+					</div>
+					<div class="span10">
+					<h4><?php echo $obj['activity_name'] ?></h4>
+					<p></p>
+					<p><span class="label label-info">活动简介</span></p>
+					<p><?php echo $obj['activity_describe'] ?></p>
+					<p style="text-align:right"><a class="btn btn-success" href="./group/1/单页/show.php?id=<?php echo $id?>"><i class="icon-star icon-white"></i>了解详情</a></p>
+					</div>
+				</div>
+				<?php }?>
+				<div class="pagination" style="text-align:center">
+					<ul>
+					   <li><a><?php if($totalpage!=0) echo $page. "/" .$totalpage;
+                         else echo "暂无活动"; ?></a></li>
+					   
+				       <li><?php if($page==1 || $page > $totalpage) {?><a >首页</a>
+					   <?php } else {?><a href="join.php?location=2&&page=1">首页</a><?php }?>
+					   </li>
+					   <li><?php if($page==1 || $page > $totalpage) {?><a >上一页</a>
+					   <?php } else {?><a href="join.php?location=2&&page=<?php echo $page-1 ?>">上一页</a><?php }?>
+					   </li>
+					   <li><?php if($page==$totalpage || $page > $totalpage) { ?><a >下一页</a>
+					   <?php } else {?><a href="join.php?location=2&&page=<?php echo $page+1 ?>">下一页</a><?php }?>
+					   </li>
+					   <li><?php if($page==$totalpage || $page > $totalpage) { ?><a >尾页</a>
+					   <?php } else {?><a href="join.php?location=2&&page=<?php echo $totalpage ?>">尾页</a><?php }?>
+					   </li>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
+	<div class="navbar">
+	  <div class="navbar-inner">
+	      <p style="text-align:center">版权@大学生拼拼平台</p>
+	  </div>
+	</div>
+</html>
