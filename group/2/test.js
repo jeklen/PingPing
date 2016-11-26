e1de50ec790e457c5f365d90b26bf2c70df78872
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-26 21:42:11 +0800

    delete others

--- group/2/test.js
+++
@@ -1,9 +0,0 @@
-javascript:
-changer.activity_name.value = "一起去打lol";
-changer.activity_describe.value = "五个人一起开黑，听说学校对面有一家不错的网吧";
-changer.activity_time.value = "2016/11/30 20:00";
-changer.activity_population.value = "5";
-changer.activity_place.value = "学校对面";
-changer.user_name.value = "马达";
-changer.tel.value = "15800011011";
-changer.submit.focus();
--- group/3/test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zhang
- * Date: 2016/11/25
- * Time: 21:51
- */
