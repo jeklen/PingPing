c174885b60fe2b2c41b941d96f7f7d72f857b293
zhql0907@outlook.com <zhql0907@outlook.com>
2016-11-26 16:17:36 +0800

    修改affectedRow函数拼写错误

--- diangezan.php
+++ diangezan.php
@@ -52,7 +52,7 @@ echo "<br>";
             <?php
             $sql = "select * from zanshu";
             $result = $mysql->runSql($sql);
-            echo $mysql-> afftctedRows();
+            echo $mysql-> affectedRows();
             ?>
             ?>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, iusto, unde, sunt incidunt id sapiente rerum soluta voluptate harum veniam fuga odit ea pariatur vel eaque sint sequi tenetur eligendi.</p>
