4ebd628b4fcca06de6ad65928b517cb7ea0ce5c0
wangshuai97 <839414890@qq.com>
2016-11-30 21:17:09 +0800

    debug

--- deleteJoinedActivity.php
+++ deleteJoinedActivity.php
@@ -3,14 +3,17 @@
 	if(!isset($_SESSION['user_id'])){
 		echo "<script>alert('请从微信端进入');history.back();</script>";
 	}
-	else{ $user_id=$_SESSION['user_id'];
-    if(isset($_GET['id']) && is_numeric($_GET['id'])){
-		$show_id=$_GET['id'];}
-	else{
-		echo'<script type="text/javascript">location.href="../../../join.php"</script>';
-	}
-	$link=new SaeMysql();
-	$sql4="delete from activity_user_joiner where activity_id='$id' and joiner_id='$user_id'";
-	$link->runSql($sql4);
-	echo'<script type="text/javascript">location.href="../../../join.php"</script>';		
+	else{ 
+		$user_id=$_SESSION['user_id'];
+		if(isset($_GET['id']) && is_numeric($_GET['id'])){
+			$show_id=$_GET['id'];
+		}
+		else{
+			echo'<script type="text/javascript">location.href="../../../join.php"</script>';
+		}
+		$link=new SaeMysql();
+		$sql4="delete from activity_user_joiner where activity_id='$id' and joiner_id='$user_id'";
+		$link->runSql($sql4);
+		echo'<script type="text/javascript">location.href="../../../join.php"</script>';	
+	}	
 ?>
--- join_action.php
+++ join_action.php
@@ -4,22 +4,24 @@
 	if(!isset($_SESSION['user_id'])){
 		echo "<script>alert('请从微信端进入');history.back();</script>";
 	}
-	else{ $user_id=$_SESSION['user_id'];
-	$id=$_GET['id'];
-	$link=new SaeMysql();
-	$check_sql="select * from activity_user_joiner where activity_id='$id' and joiner_id='$user_id'";
-	$check=$link->getData($check_sql);
-	if($check){
-		echo "<script>alert('您已参加，请勿重复报名！');history.back();</script>";
+	else{ 
+		$user_id=$_SESSION['user_id'];
+		$id=$_GET['id'];
+		$link=new SaeMysql();
+		$check_sql="select * from activity_user_joiner where activity_id='$id' and joiner_id='$user_id'";
+		$check=$link->getData($check_sql);
+		if($check){
+			echo "<script>alert('您已参加，请勿重复报名！');history.back();</script>";
+		}
+		else{
+			$sql="insert into activity_user_joiner (activity_id,joiner_id) values('$id','$user_id')";
+			$link->runSql($sql);
+			if($link->errno()==0){
+				echo "<script>alert('报名成功');location.href='./show.php?id=$id';</script>";
+			}
+			else die("error" . $link->errmsg());
+		}
 	}
-	else{
-	    $sql="insert into activity_user_joiner (activity_id,joiner_id) values('$id','$user_id')";
-		$link->runSql($sql);
-		if($link->errno()==0){
-		echo "<script>alert('报名成功');location.href='./show.php?id=$id';</script>";
-	    }
-        else die("error" . $link->errmsg());
-    }}
 ?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
