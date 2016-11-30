a07e8c7701a062912db9aba7c7a3e59ad492ff39
wangshuai97 <839414890@qq.com>
2016-11-30 20:12:01 +0800

    delete joined activity

--- deleteJoinedActivity.php
+++ deleteJoinedActivity.php
@@ -1,11 +1,16 @@
 <?php
+	session_start();
+	if(!isset($_SESSION['user_id'])){
+		echo "<script>alert('请从微信端进入');history.back();</script>";
+	}
+	else{ $user_id=$_SESSION['user_id'];
     if(isset($_GET['id']) && is_numeric($_GET['id'])){
-		$user_id=$_GET['id'];}
+		$show_id=$_GET['id'];}
 	else{
 		echo'<script type="text/javascript">location.href="../../../join.php"</script>';
 	}
 	$link=new SaeMysql();
-	$sql4="delete from activity_user_joiner where joiner_id='$user_id'";
+	$sql4="delete from activity_user_joiner where where activity_id='$id' and joiner_id='$user_id'";
 	$link->runSql($sql4);
-	echo'<script type="text/javascript">location.href="../../../join.php"</script>';	
+	echo'<script type="text/javascript">location.href="../../../join.php"</script>';		
 ?>
